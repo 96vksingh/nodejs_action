4ad66ba1ae4c2c3687f767fa08b8c8b11aea0430
Vishal Kumar Singh <01vksingh@gmail.com>
2020-03-20 21:09:37 +0530

    added modules

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+var ip = require('ip');
+var geoip = require('geoip-lite');
 
+var ip = ip.address()
+var ipinf = geoip.lookup(ip);
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  core.setOutput("triggered ip information:",ipinf)
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  
 } catch (error) {
   core.setFailed(error.message);
 }
