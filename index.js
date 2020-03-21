426dadc2ba82c40a744dcdadfc3cb4b2156617d8
Vishal Kumar Singh <01vksingh@gmail.com>
2020-03-22 00:16:05 +0530

    done few things

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ try {
 // console.log(ipinf)
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    core.setOutput("triggered ip information:",ipinf)
+    core.setOutput("ipinf",ipinf)
     
     }
 
