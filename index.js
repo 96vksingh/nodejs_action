17205e940b230a19e366dc149236552ec649fe02
Vishal Kumar Singh <01vksingh@gmail.com>
2020-03-21 23:47:37 +0530

    done

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 var ip = require('ip');
 var geoip = require('geoip-lite');
+const publicIp = require('public-ip');
+global.ip=""
 
-var ip = ip.address()
-var ipinf = geoip.lookup(ip);
 try {
-  // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  core.setOutput("triggered ip information:",ipinf)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  
+
+  (async () => {
+    // var ipinf = console.log(await publicIp.v4());
+    if(ip = await publicIp.v4()){
+      console.log("in")
+      console.log(ip);
+      console.log("last")
+      let ips = geoip.lookup(ip);
+      console.log(ips)
+      var ipinf = JSON.stringify(ips);
+    console.log(ipinf)
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    core.setOutput("triggered ip information:",ipinf)
+    }
+    
+    // ipv(ip);
+
+    
+    
+
+
+})();
+
 } catch (error) {
   core.setFailed(error.message);
 }
