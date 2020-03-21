1ebc1eb149edbdd75a309cc51b25d27cd2776a28
Vishal Kumar Singh <01vksingh@gmail.com>
2020-03-22 00:53:27 +0530

    done few things

--- index.js
+++ index.js
@@ -3,8 +3,35 @@ const github = require('@actions/github');
 var ip = require('ip');
 var geoip = require('geoip-lite');
 const publicIp = require('public-ip');
+const speedTest = require('speedtest-net');
 global.ip=""
 global.ipin=""
+const { exec } = require("child_process");
+exec("npm install --global speedtest-net", (error, stdout, stderr) => {
+  if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+  }
+  if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
+exec("speedtest-net --accept-license", (error, stdout, stderr) => {
+  if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+  }
+  if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
+
 try {
 
   (async () => {
@@ -29,7 +56,19 @@ try {
 
 })();
 
+(async () => {
+  try {
+    console.log(await speedTest(acceptLicense=true));
+  } catch (err) {
+    console.log(err.message);
+  } finally {
+    process.exit(0);
+  }
+})();
+
 
 } catch (error) {
   core.setFailed(error.message);
 }
+
+
